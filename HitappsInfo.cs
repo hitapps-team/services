106718b238e717abd08b71bdcbabb9bd8252d3a8
Artem Bayer <artem.b@hitapps.com>
2020-11-30 16:01:04 +0300

    Up to V0.0.2

---
+++ HitappsInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace com.hitapps.services
+{
+    public class HitappsInfo
+    {
+        public string DeviceOrientation()
+        {
+            return Input.deviceOrientation.ToString();
+        }
+
+        public bool IdfaTrackingIsLimited()
+        {
+            return false;
+        }
+
+        public string DeviceConnectionType()
+        {
+            return Application.internetReachability.ToString();
+        }
+    }
+}
