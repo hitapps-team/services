74cf5de34c4772ab6ff1eaf9fb97222f3727ab99
Artem Bayer <artem.b@hitapps.com>
2020-11-30 16:01:04 +0300

    Up to V0.0.2

--- Runtime/IAdService.cs
+++ Runtime/IAdService.cs
@@ -1,45 +1,16 @@
 using System;
-using com.hitapps.services.data;
 
 namespace com.hitapps.services
 {
-    public enum UnitStatus
-    {
-        None = 0,
-        Requesting = 1,
-        Failed = 3,
-        Ready = 4
-    }
-
     public interface IAdService : IService
     {
-        event Action<string> InterstitialTryToShow;
-        event Action<string> RewardedTryToShow;
-        event Action<string, bool> InterstitialShown;
-        event Action<string, bool> RewardedShown;
-        event Action<string,UnitStatus> StatusCnahged;
-        bool IsReady(string unitId);
-        void Enable();
-        void Disable();
-        event Action SdkInitialized;
-        bool Initialised { get; }
-        void Init();
-        void ShowBanner<T>(string id, T settings);
-        void HideBanner(string id);
-        void ShowInterstitial<T>(string id, T settings);
-        void ShowRewarded<T>(string id, T settings);
-        void ShowBanner(string id);
-        void ShowInterstitial(string id);
-        void ShowRewarded(string id);
-        void RequestBanner(string id);
-        void RequestInterstitial(string id);
-        void RequestRewarded(string id);
-        void LoadConsentDialog();
-        void ShowConsentDialog();
-        void ForceGdprApplicable();
-        void GrantConsent();
-        void RevokeConsent();
-        void ReportApplicationOpen();
-        void EnableLocationSupport(bool enable);
+        bool BannerReady { get; }
+        bool RewardedReady { get; }
+        bool InterReady { get; }
+        void Run();
+        bool ShowRewardedVideo(string placement, Action<bool> onRewardedShown);
+        void ShowInterstitial(string placement, Action call = null);
+        void ShowBanner();
+        void HideBanner();
     }
 }
--- Runtime/Stub/AdServiceStub.cs
+++ Runtime/Stub/AdServiceStub.cs
@@ -1,122 +1,36 @@
 using System;
-using com.hitapps.services.data;
 
 namespace com.hitapps.services.Stub
 {
     public class AdServiceStub : IAdService
     {
-        public event Action<string> InterstitialTryToShow;
-        public event Action<string> RewardedTryToShow;
-        public event Action<string, bool> InterstitialShown;
-        public event Action<string, bool> RewardedShown;
-        public event Action<string,UnitStatus> StatusCnahged;
+        public bool BannerReady { get; }
+        public bool RewardedReady { get; }
+        public bool InterReady { get; }
 
-        public bool IsReady(string unitId)
+        public void Run()
         {
-            return true;
+            //  throw new NotImplementedException();
         }
 
-        public void Enable()
+        public bool ShowRewardedVideo(string placement, Action<bool> onRewardedShown)
         {
-            throw new NotImplementedException();
+            return false;
         }
 
-        public void Disable()
+        public void ShowInterstitial(string placement, Action call = null)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
         }
 
-        public event Action SdkInitialized;
-        public bool Initialised => true;
-
-        public void Init()
+        public void ShowBanner()
         {
-            SdkInitialized?.Invoke();
+            // throw new NotImplementedException();
         }
 
-        public void ShowBanner<T>(string id, T settings)
+        public void HideBanner()
         {
-            throw new NotImplementedException();
-        }
-
-        public void HideBanner(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ShowInterstitial<T>(string id, T settings)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ShowRewarded<T>(string id, T settings)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ShowBanner(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ShowInterstitial(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ShowRewarded(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RequestBanner(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RequestInterstitial(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RequestRewarded(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void LoadConsentDialog()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ShowConsentDialog()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ForceGdprApplicable()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GrantConsent()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RevokeConsent()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ReportApplicationOpen()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void EnableLocationSupport(bool enable)
-        {
-            throw new NotImplementedException();
+            //   throw new NotImplementedException();
         }
     }
 }
