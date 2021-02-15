f98c3f87c018e6c59e521803fc6b30cbd2fbd5c5
Artem Bayer <artem.b@hitapps.com>
2021-02-15 13:33:36 +0300

    Analytics tracker for ads

---
+++ Runtime/data/IAdsAnalyticsTracker.cs
@@ -0,0 +1,9 @@
+﻿namespace com.hitapps.services.data
+{
+    public interface IAdsAnalyticsTracker
+    {
+        void TrackBannerNeeded(string placement);
+        void TrackInterNeeded(string placement);
+        void TrackRewardedNeeded(string placement);
+    }
+}
